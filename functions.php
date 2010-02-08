00b6fd33bacb164130509aaad93f2e9cf178f9db
Hiski <hiisty@gmail.com>
2010-02-08 23:57:21 +0200

    jotain säätöö

--- functions.php
+++ functions.php
@@ -3,8 +3,8 @@
 function print_header()
 {
 		$content = '
-                <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
-				"http://www.w3.org/TR/html4/strict.dtd">
+                <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+				"http://www.w3.org/TR/html4/loose.dtd">
 				<HTML>
 						<HEAD>
                         <meta http-equiv="Content-type" content="text/html;charset=UTF-8" >
